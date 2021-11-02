b56b8c6aae39be31e91ce48a71db323ef7092504
itsMao <45157916+itsMao@users.noreply.github.com>
2021-11-02 17:23:11 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   github: 'itsMao',
-  linkedin: 'no',
+
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
